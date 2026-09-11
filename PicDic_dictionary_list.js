c26047c84f2e9b7e07613bac48764586a8243ee9
chigre <guangdong.hu@gmail.com>
2026-09-11 17:18:11 +0800

    Add files via upload
    example dictionary list

---
+++ PicDic_dictionary_list.js
@@ -0,0 +1,37 @@
+// ===== 自动生成的词典列表 =====
+var picdic_dictList = {
+    'PicDic_EN-EN_CENTURY': {
+        name: "The Century Dictionary",
+        index_language: "eng",
+        contents_language: "eng",
+        indexPath: "PicDic_EN-EN_CENTURY/PicDic_EN-EN_CENTURY_index.js",
+        version: "1",
+        resourceId: "bda20387733be287d41366c72bd3e53e"
+    },
+    'PicDic_EN-ZH_FarEastECD': {
+            name: '遠東英漢大辭典 Far East English-Chinese Dictionary',
+            index_language: 'eng',
+            contents_language: 'zho',
+            indexPath: 'PicDic_EN-ZH_FarEastECD/PicDic_EN-ZH_FarEastECD_index.js',
+            version: '1',
+            resourceId: '44ca71254349232db988226f61be9ebb'
+        },
+    'PicDic_ES-ZH_NEraGDEC': {
+            name: '新时代西汉大词典 Nueva Era Gran Diccionario Español-Chino',
+            index_language: 'spa',
+            contents_language: 'zho',
+            indexPath: 'PicDic_ES-ZH_NEraGDEC/PicDic_ES-ZH_NEraGDEC_index.js',
+            version: '1',
+            resourceId: 'd8b78f0c794016d04dfe6bbf6622cf55'
+        },
+    'PicDic_PT-ZH_DPC': {
+        name: '葡汉词典 DICIONÁRIO PORTUGUÊS-CHINÊS',
+        index_language: 'por',
+        contents_language: 'zho',
+        indexPath: 'PicDic_PT-ZH_DPC/PicDic_PT-ZH_DPC_index.js',
+        version: '1',
+        resourceId: '22ac9d26f3de9d5a071b0df02b19c655'
+    }
+    };
+window.picdic_dictList = picdic_dictList;
+console.log('[PicDic] 词典列表已注册，共 ' + Object.keys(picdic_dictList).length + ' 个词典。');
